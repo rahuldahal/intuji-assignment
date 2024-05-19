0b739136891abd54459554256b8bed354d108b9d
rahuldahal <rdaahal@gmail.com>
2024-05-19 14:13:53 +0545

    feat: handle callback

---
+++ calendar.php
@@ -0,0 +1,1 @@
+<h1>Handle Calendar Lists</h1>
--- callback.php
+++ callback.php
@@ -1,1 +1,30 @@
-<h1>Handle Callback now!</h1>
+<?php
+
+require("utils/api.php");
+
+use Google\Service\Calendar;
+
+if (isset($_GET['code'])) {
+  $token = $client->fetchAccessTokenWithAuthCode($_GET['code']);
+  if (isset($token['error'])) {
+      echo 'Oops!! Authorization failed: ' . $token['error'];
+      exit;
+  }
+
+  $client->setAccessToken($token);
+  $_SESSION['access_token'] = $token;
+
+  $calendarService = new Calendar($client); // Here, Calendar = Google\Service\Calendar
+
+  // get calendar list from API
+  $calendarList = $calendarService->calendarList->listCalendarList();
+
+  // store data in the "user_calendars" session
+  $_SESSION['user_calendars'] = $calendarList->getItems();
+
+  header('Location: calendar.php');
+  exit;
+} else {
+  echo 'Oops!! Authorization failed.';
+}
+?>
